d4c3c67638f0d639cddb8e64f6bea03159d60388
Analía <variedadesxxi@hotmail.com>
2022-10-19 00:42:45 -0300

    cambios security

---
+++ src/main/java/com/portfolio/ap/Security/Enums/RolNombre.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.ap.Security.Enums;
+
+/**
+ *
+ * @author Analía
+ */
+public enum RolNombre {
+    ROLE_ADMIN,
+
+    /**
+     *
+     */
+    ROLE_USER
+    
+}
